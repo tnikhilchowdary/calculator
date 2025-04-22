3452dc112ccdd787b58f61a6dfcf4ea4d2d47f32
tnikhilchowdary <tnikilchowdary@gmail.com>
2025-04-21 19:54:10 -0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [num1, setnum2] = use
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
